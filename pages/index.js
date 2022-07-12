4b9c6d2b4ef35340f99094f60f9cece99473f448
sfujimotosfujimoto <unicoshun@gmail.com>
2022-07-13 07:30:41 +0900

    finish google homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
+import { MicrophoneIcon, SearchIcon } from "@heroicons/react/solid"
+
 import Head from "next/head"
 import Header from "../components/Header"
+import Image from "next/image"
 
 export default function Home() {
   return (
@@ -12,6 +15,24 @@ export default function Home() {
       {/* Header */}
       <Header />
       {/* Body */}
+      <form className="flex flex-col items-center mt-40">
+        <Image
+          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c51f.png"
+          objectFit="cover"
+          width={300}
+          height={100}
+          alt="logo"
+        />
+        <div className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl">
+          <SearchIcon className="h-5 text-gray-500 mr-3" />
+          <input type="text" className="flex-grow focus:outline-none" />
+          <MicrophoneIcon className="h-5 text-gray-500" />
+        </div>
+        <div className="flex flex-col sm:flex-row w-1/2 space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
+          <button className="btn">Google Search</button>
+          <button className="btn">I&apos;m feeling lucky</button>
+        </div>
+      </form>
 
       {/* Footer */}
     </div>
