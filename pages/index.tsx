c42bef16598c4c181964571d0f6de59d963e1069
sfujimotosfujimoto <unicoshun@gmail.com>
2022-07-14 07:05:15 +0900

    add router to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,23 @@
 import { MicrophoneIcon, SearchIcon } from "@heroicons/react/solid"
+import React, { useRef } from "react"
 
 import Footer from "../components/Footer"
 import Head from "next/head"
 import Header from "../components/Header"
 import Image from "next/image"
+import { useRouter } from "next/router"
 
 export default function Home(): JSX.Element {
+  const router = useRouter()
+  const searchInputRef = useRef(null)
+
+  function search(event: MouseEvent) {
+    event.preventDefault()
+    const term = searchInputRef.current.value
+    if (!term.trim()) return
+    router.push(`/search?term=${term.trim()}`)
+  }
+
   return (
     <div>
       <Head>
@@ -26,11 +38,17 @@ export default function Home(): JSX.Element {
         />
         <div className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl">
           <SearchIcon className="h-5 text-gray-500 mr-3" />
-          <input type="text" className="flex-grow focus:outline-none" />
+          <input
+            ref={searchInputRef}
+            type="text"
+            className="flex-grow focus:outline-none"
+          />
           <MicrophoneIcon className="h-5 text-gray-500" />
         </div>
         <div className="flex flex-col sm:flex-row w-1/2 space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
-          <button className="btn">Google Search</button>
+          <button onClick={search} className="btn">
+            Google Search
+          </button>
           <button className="btn">I&apos;m feeling lucky</button>
         </div>
       </form>
